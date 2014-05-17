3adad66d3a5f70dd3e6a14c8f5cbdb419272d10b
martinqt <m.ki2@laposte.net>
2014-05-17 11:18:21 +0200

    Use config file

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <QtQuick/QQuickView>
 #include <QtGui/QGuiApplication>
+#include <QSettings>
 #include "commandmodel.h"
 #include "commandwindow.h"
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
-    QString exePath = app.applicationDirPath();
-    QDir work = QDir(exePath);
-    work.cd("../");
-    QDir console = QDir(exePath);
-    console.cd("../app");
+    QSettings settings("config.ini", QSettings::IniFormat);
+    QString work = settings.value("Directories/work", "default").toString();
+    QString console = settings.value("Directories/console", "default").toString();
+
+    if(work == "default") {
+        QString exePath = app.applicationDirPath();
+        QDir workDir = QDir(exePath);
+        workDir.cd("../");
+        work = workDir.absolutePath();
+    }
+    if(console == "default") {
+        QString exePath = app.applicationDirPath();
+        QDir consoleDir = QDir(exePath);
+        consoleDir.cd("../app");
+        console = consoleDir.absoluteFilePath("console");
+    }
 
     CommandWindow *comWin = new CommandWindow();
-    comWin->process(work.absolutePath(), console.absoluteFilePath("console"));
+    comWin->process(work, console);
 
     return app.exec();
 }
